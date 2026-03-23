b23b27271c9afce9f772cd29539a23faf66f7c34
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2026-03-23 16:10:11 +0530

    removed otp system temporarily

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,56 +9,56 @@ export default function Signup() {
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
-    const [otp, setOTP] = useState("");
-    const [isVerified, setIsVerified] = useState(false);
+    // const [otp, setOTP] = useState("");
+    // const [isVerified, setIsVerified] = useState(false);
 
-    const [loadingOTP, setLoadingOTP] = useState(false);
-    const [loadingVerify, setLoadingVerify] = useState(false);
+    // const [loadingOTP, setLoadingOTP] = useState(false);
+    // const [loadingVerify, setLoadingVerify] = useState(false);
     const [loadingSignup, setLoadingSignup] = useState(false);
 
-    const sendOTP = async () => {
-        try {
-            setLoadingOTP(true);
-            const res = await API.post('/auth/signup', { email, password, name });
-            alert(res.data.message);
-            setIsVerified(false);
-            setOTP('');
-        } catch (err) {
-            alert("Error sending OTP");
-            console.log(err);
-        } finally {
-            setLoadingOTP(false);
-        }
-    };
+    // const sendOTP = async () => {
+    //     try {
+    //         setLoadingOTP(true);
+    //         console.log("DATA BEING SENT:", { email, password, name });
+    //         const res = await API.post('/auth/signup', { email, password, name });
+    //         alert(res.data.message);
+    //         setIsVerified(false);
+    //         setOTP('');
+    //     } catch (err) {
+    //         alert("Error sending OTP");
+    //         console.log(err);
+    //     } finally {
+    //         setLoadingOTP(false);
+    //     }
+    // };
 
-    const verifyOTP = async () => {
-        try {
-            setLoadingVerify(true);
-            const res = await API.post('/auth/verify-otp', { email, otp });
-            alert(res.data.message);
-            setIsVerified(true);
-        } catch (err) {
-            alert("Error verifying OTP");
-        } finally {
-            setLoadingVerify(false);
-        }
-    }
+    // const verifyOTP = async () => {
+    //     try {
+    //         setLoadingVerify(true);
+    //         const res = await API.post('/auth/verify-otp', { email, otp });
+    //         alert(res.data.message);
+    //         setIsVerified(true);
+    //     } catch (err) {
+    //         alert("Error verifying OTP");
+    //     } finally {
+    //         setLoadingVerify(false);
+    //     }
+    // }
 
     const handleSignup = async () => {
-        if (!isVerified) {
-            alert("Please verify OTP first");
-            return;
-        }
-
-        try {
+        try{
             setLoadingSignup(true);
+            const res = await API.post('/auth/signup', { email, password, name });
+            alert(res.data.message);
             navigate('/login');
+        } catch(err){
+            alert("Error signing up");
         } finally {
             setLoadingSignup(false);
         }
     }
 
-    if(loadingOTP || loadingVerify || loadingSignup){
+    if(loadingSignup){
         return <Loader />;
     }
 
@@ -84,23 +84,23 @@ export default function Signup() {
                         <label htmlFor="password" className="text-(--text)">Password: </label>
                         <input type="text" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="border border-white bg-transparent text-white placeholder:text-white p-3 rounded-2xl shadow-xl/50 shadow-white transition-all duration-400 mb-2" />
                     </div>
-
+{/* 
                     <div className="flex flex-col gap-2 md:w-[40%]">
                         <label htmlFor="otp" className="text-(--text)">OTP: </label>
                         <input type="text" id="otp" placeholder="OTP" value={otp} onChange={(e) => setOTP(e.target.value)} className="border border-white bg-transparent text-white placeholder:text-white p-3 rounded-2xl shadow-xl/50 shadow-white transition-all duration-400 mb-2 " />
-                    </div>
+                    </div> */}
 
                     
                 </div>
 
                 <div className="flex flex-col md:flex-row md:gap-5 md:w-[60%] w-full px-10 justify-around">
-                    <button onClick={sendOTP} className="bg-(--secondary) px-5 py-2 rounded-2xl mt-5 hover:shadow-xl/30 shadow-white transition-all duration-400 text-white">
+                    {/* <button onClick={sendOTP} className="bg-(--secondary) px-5 py-2 rounded-2xl mt-5 hover:shadow-xl/30 shadow-white transition-all duration-400 text-white">
                         {loadingOTP ? "Sending OTP..." : "Send OTP"}
                     </button>
 
                     <button onClick={verifyOTP} className="bg-(--secondary) px-5 py-2 rounded-2xl mt-5 hover:shadow-xl/30 shadow-white transition-all duration-400 text-white">
                         {loadingVerify ? "Verifying OTP..." : "Verify OTP"}
-                    </button>
+                    </button> */}
 
                     <button onClick={handleSignup} className="bg-(--secondary) px-5 py-2 rounded-2xl mt-5 hover:shadow-xl/30 shadow-white transition-all duration-400 text-white">
                         {loadingSignup ? "Signing up..." : "Signup"}
