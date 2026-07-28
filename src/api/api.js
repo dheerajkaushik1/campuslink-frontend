720a4bc4126ae41f61b60359cae27ef9178c8818
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2026-07-28 20:46:37 +0530

    Bug Fixes

--- src/api/api.js
+++ src/api/api.js
@@ -1,26 +1,25 @@
-import axios from 'axios';
-import { response } from 'express';
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'https://campuslink-backend-wv2h.onrender.com/api',
+    baseURL: "https://campuslink-backend-wv2h.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
-    if (token){
+    if (token) {
         req.headers.Authorization = `Bearer ${token}`;
     }
+
     return req;
 });
 
 API.interceptors.response.use(
     (response) => response,
     (error) => {
-        if(error.response?.status === 401){
+        if (error.response?.status === 401) {
             localStorage.removeItem("token");
             alert("Session expired. Please login again.");
-
             window.location.href = "/login";
         }
 
