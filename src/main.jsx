4ff77b2e352cef984466664ac2485c4cadc10e9c
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2026-08-08 11:19:41 +0530

    Owner Verification

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { StrictMode } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+    <Analytics />
+  </StrictMode>
 )
